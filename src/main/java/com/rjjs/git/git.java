3219ae5a8e007e41bf4d769d42abe4c1bfa8667d
Zyu <1485673686@qq.com>
2023-10-25 10:46:48 +0800

    second commit

--- src/main/java/com/rjjs/git/git.java
+++ src/main/java/com/rjjs/git/git.java
@@ -3,5 +3,7 @@ package com.rjjs.git;
 public class git {
     public static void main(String[] args) {
         System.out.printf("hello git");
+        System.out.printf("hello git1");
+        System.out.printf("hello git2");
     }
 }
